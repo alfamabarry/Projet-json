b991d55d47e9bfaf995e0fd64cf718a2a13e8395
alfamabarry <92032405+alfamabarry@users.noreply.github.com>
2022-02-04 12:38:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){                           
+    $.getJSON("produit.json", function(data){
+     var tab = '';
+     
+     $.each(data, function(key, value){
+         tab +=  '<tr>';
+         tab +=  '<td>'+value.id+'</td>';
+         tab +=  '<td>'+value.designation+'</td>';
+         tab +=  '<td>'+value.prix+'</td>';
+         tab +=  '<td>'+value.catégorie+'</td>';
+         tab +=  '<td>'+value.disponibilite+'</td>';
+         tab +=  '<td>'+"RaisonSocial: "+value.fournisseur.raisonSocial+'<br>'+"Adress: "+value.fournisseur.adress+'</td>';
+         tab +=  '</tr>';
+     });
+     $('#tablee').append(tab);
+    });
+});
+
+$(document).ready(function(){                          
+    $("#search").on("keyup",function(){
+var value = $(this).val().toLowerCase();
+
+    $("#tBody tr").filter(function(){
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1  )
+    });
+    
+   } );
+
+});
+
+//sort
+
+$('th').click(function(){
+  var table = $(this).parents('table').eq(0)
+  var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+  this.asc = !this.asc
+  if (!this.asc){rows = rows.reverse()}
+  for (var i = 0; i < rows.length; i++){table.append(rows[i])}
+})
+
+
+function comparer(index) {
+  return function(a, b) {
+      var valA = getCellValue(a, index), valB = getCellValue(b, index)
+      return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+  }
+}
+function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
